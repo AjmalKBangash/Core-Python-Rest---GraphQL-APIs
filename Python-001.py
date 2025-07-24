3791067dcfb525ccfc5977aa53c484dd57dea43e
AjmalKBangash <ak1489007@gmail.com>
2025-07-24 18:01:42 +0500

    iterators, decorators and generators are practiced

---
+++ Python-001.py
@@ -0,0 +1,11 @@
+# python operators ---> **,//,%
+# logical operators ---> and,not,or
+# ternary operator ---> (x if condition else y)
+# diff btw continue and pass in loops
+# sets {} ??? why curly braces
+#  function parameters and its arguments
+# upto functions and python partial functions
+
+
+result = isinstance(3.14, int)  # True — matches one of the types
+print(result)
