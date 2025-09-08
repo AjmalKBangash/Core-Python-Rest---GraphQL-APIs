6832d98033ca8c3bcc8051afad4513072fad1b07
AjmalKBangash <ak1489007@gmail.com>
2025-09-08 22:08:43 +0300

    recursive calling

--- Decorators.py
+++ Decorators.py
@@ -13,3 +13,7 @@ def say_hi():
     print("Hi!")
 
 say_hi()  # Prints Hi! three times
+say_hi()  # Prints Hi! three times
+say_hi()  # Prints Hi! three times
+say_hi()  # Prints Hi! three times
+say_hi()  # Prints Hi! three times
